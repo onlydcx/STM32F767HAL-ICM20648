69010e00dc8406da0355292345d7ca70f173c67a
onlydcx <rin.hanaoka@icloud.com>
2024-02-06 23:56:56 +0900

    create app_main

---
+++ Core/App/app_main.cpp
@@ -0,0 +1,15 @@
+/*
+ *  app_main.h
+ *
+ *  Created on: Feb 6, 2024
+ * 
+ *  Author: onlydcx
+ */
+
+#include "app_main.h"
+
+void app_main() {
+    while(1) {
+        
+    }
+}
---
+++ Core/App/app_main.hpp
@@ -0,0 +1,20 @@
+/*
+ *  app_main.h
+ *
+ *  Created on: Feb 6, 2024
+ * 
+ *  Author: onlydcx
+ */
+
+#ifndef __APP_APP_MAIN_H__
+#define __APP_APP_MAIN_H__
+
+#ifdef __cplusplus
+extern "C" {
+void app_main();
+};
+#else
+void app_main();
+#endif
+
+#endif /* __APP_APP_MAIN_H__ */
